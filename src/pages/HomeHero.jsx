eb856b9c9dcf98868e7638ef2918b61b3d2aaf43
Rahul Misal <codesrahul96@gmail.com>
2025-04-03 14:07:13 +0530

    updated hover effects for cards

--- src/pages/HomeHero.jsx
+++ src/pages/HomeHero.jsx
@@ -312,7 +312,7 @@ const HomeHero = () => {
         <div className="flex flex-col md:flex-row gap-8 items-top ">
           <div className="w-full md:w-1/2">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.835434509372!2d144.9537353153167!3d-37.81627927975195!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11fd81%3A0xf577d1c4b1b1c4b!2sMelbourne%20VIC%2C%20Australia!5e0!3m2!1sen!2sus!4v1635781234567!5m2!1sen!2sus"
+              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1083.5912749774066!2d73.8544322942217!3d18.463534172952826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1743669345038!5m2!1sen!2sin"
               width="100%"
               height="400"
               style={{ border: 0 }}
